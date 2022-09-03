58100eca8c0271e17419e09ef4e8dea996547a6e
jonaszimber <55903865+jonaszimber@users.noreply.github.com>
2022-09-03 11:39:43 -0300

    Update mario_pyramid.c

--- C/mario_pyramid.c
+++ C/mario_pyramid.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 int main() {
-    int line, column;
+
+    int line, column, num;
+    printf("\n\n ENTER A NUMBER BETWEEN 1 AND 8:");
+    scanf("%d",&num);
+    while(num<1||num>8)
+    {
+    printf("\n THE NUMBER ENTERED IS INCORRECT. TYPE IT AGAIN:");
+    scanf("%d",&num);
+    }
+    printf("\n\n THE NUMBER ENTERED IS:% d",num);
+    printf("\n\n");
     for (line=1; line<10; line++) {
-        for(column=0; column<=7; column++)
+        for(column=0; column<=num; column++)
             printf(column<9-line?" ":"#");
         printf("\n");
     }
-}
+    }
